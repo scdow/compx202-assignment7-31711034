cfb2b6731b9ca990f8d87de2e729f61e82d013d8
scdow <920301714@qq.com>
2020-05-28 13:26:12 +0800

    push empty project

---
+++ app/src/main/java/com/example/compx202_assignment7_31711034/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.compx202_assignment7_31711034;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
