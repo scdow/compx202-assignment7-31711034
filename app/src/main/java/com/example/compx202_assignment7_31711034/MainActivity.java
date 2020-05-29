2b7a810c44917252e082f4c9b9ef258c10c07934
scdow <920301714@qq.com>
2020-05-29 10:24:01 +0800

    Sticky Immersive full screen mode

--- app/src/main/java/com/example/compx202_assignment7_31711034/MainActivity.java
+++ app/src/main/java/com/example/compx202_assignment7_31711034/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.compx202_assignment7_31711034;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // Set sticky immersive fullscreen mode
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
 }
